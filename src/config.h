adfe4dfe0fdab127fa96656761c9be137cc1a6fb
David Wedel <davidwedel06@gmail.com>
2026-01-10 16:46:02 -0600

    get relay pins actually right

--- src/config.h
+++ src/config.h
@@ -7,15 +7,15 @@
 // Network Configuration
 #define USE_ETHERNET
 
-// Relay pins (LilyGo 8-channel board)
-#define RELAY_1_PIN 33  // Auger (swapped - physical wiring was backwards)
-#define RELAY_2_PIN 32  // Chain A
-#define RELAY_3_PIN 25  // Chain B
-#define RELAY_4_PIN 26  // Chain C
-#define RELAY_5_PIN 27  // Chain D
-#define RELAY_6_PIN 14
-#define RELAY_7_PIN 12
-#define RELAY_8_PIN 13
+// Relay pins (LilyGo 8-channel board - correct from datasheet)
+#define RELAY_1_PIN 33 // K1 - Auger
+#define RELAY_2_PIN 32 // K2 - Chain A
+#define RELAY_3_PIN 13 // K3 - Chain B
+#define RELAY_4_PIN 12 // K4 - Chain C
+#define RELAY_5_PIN 21 // K5 - Chain D
+#define RELAY_6_PIN 19 // K6
+#define RELAY_7_PIN 18 // K7
+#define RELAY_8_PIN 5  // K8
 
 // W5500 Ethernet SPI pins (LilyGo T-Relay W5500 Shield)
 #define W5500_CS_PIN 27
@@ -25,7 +25,7 @@
 #define W5500_RESET_PIN 23
 
 // MAC address for W5500
-#define W5500_MAC { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }
+#define W5500_MAC {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}
 
 // Status LED (built-in)
 #define STATUS_LED_PIN 2
@@ -33,7 +33,7 @@
 // Network settings
 #define WEB_SERVER_PORT 80
 #define MODBUS_PORT 502
-#define BINTRAC_TIMEOUT 5000    // milliseconds
+#define BINTRAC_TIMEOUT 5000 // milliseconds
 #define BINTRAC_RETRY_DELAY 2000
 
 // BinTrac Modbus addresses
@@ -45,14 +45,16 @@
 #define MODBUS_BIN_C_ADDR 1004
 #define MODBUS_BIN_D_ADDR 1006
 #define MODBUS_ALL_BINS_ADDR 1000
-#define MODBUS_ALL_BINS_LEN 6  // Changed from 8 - this HouseLink only supports 6!
-#define MODBUS_FUNCTION_CODE 4  // Input register
+#define MODBUS_ALL_BINS_LEN                                                    \
+  6 // Changed from 8 - this HouseLink only supports 6!
+#define MODBUS_FUNCTION_CODE 4 // Input register
 
 // Feeding control constants
-#define WEIGHT_CHECK_INTERVAL 1000  // Check weight every second
-#define MIN_WEIGHT_CHANGE 0.1       // Minimum detectable weight change
-#define ALARM_CHECK_WINDOW 60000    // Check alarm condition over 1 minute
-#define EMERGENCY_STOP_WEIGHT -50.0 // Stop if weight increases (bin filling error)
+#define WEIGHT_CHECK_INTERVAL 1000 // Check weight every second
+#define MIN_WEIGHT_CHANGE 0.1      // Minimum detectable weight change
+#define ALARM_CHECK_WINDOW 60000   // Check alarm condition over 1 minute
+#define EMERGENCY_STOP_WEIGHT                                                  \
+  -50.0 // Stop if weight increases (bin filling error)
 
 // Storage
 #define CONFIG_FILE "/config.json"
@@ -61,13 +63,13 @@
 
 // Time settings
 #define NTP_SERVER "pool.ntp.org"
-#define NTP_UPDATE_INTERVAL 3600000  // Update time every hour
+#define NTP_UPDATE_INTERVAL 3600000 // Update time every hour
 
 // Watchdog
-#define WATCHDOG_TIMEOUT 30  // seconds
+#define WATCHDOG_TIMEOUT 30 // seconds
 
 // Status update intervals
-#define STATUS_UPDATE_INTERVAL 5000    // 5 seconds
-#define TELEGRAM_UPDATE_INTERVAL 1000  // 1 second (for responsive bot commands)
+#define STATUS_UPDATE_INTERVAL 5000   // 5 seconds
+#define TELEGRAM_UPDATE_INTERVAL 1000 // 1 second (for responsive bot commands)
 
 #endif // CONFIG_H
